6bed923d79ada90265f174f1bbe451deae7948b6
Samparkyn <samparkyn101@gmail.com>
2018-05-08 20:37:14 +0200

    Rename itemsList to items-list

---
+++ src/components/items-list/items-list.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Timer } from '../timer'
+
+export class ItemsList extends Component {
+  render() {
+    const { items } = this.props
+    const rows = items.map(item => {
+      return (
+        <li key={item.id}>
+          <table>
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Price</td>
+                <td>Time remaining</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td><Timer start={item.start} duration={item.duration}/></td>
+              </tr>
+            </tbody>
+          </table>
+        </li>
+      )
+    })
+
+    return (
+      <ul className="items__list">
+        {rows}
+      </ul>
+    )
+  }
+}
+
