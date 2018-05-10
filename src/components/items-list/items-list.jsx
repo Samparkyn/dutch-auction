7b3d70ef2f434f0ac767cc516c61fce51c901618
Samparkyn <samparkyn101@gmail.com>
2018-05-10 10:55:14 +0200

    Add filters to itemsList

--- src/components/items-list/items-list.jsx
+++ src/components/items-list/items-list.jsx
@@ -1,33 +1,65 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Timer } from '../timer'
+import { ItemCard } from '../item-card'
 
 export class ItemsList extends Component {
+  static getDerivedStateFromProps(newProps, oldState) {
+    const currentFilter = oldState.filter
+
+    let filteredItems
+    switch (currentFilter) {
+      case 'all':
+        filteredItems = newProps.items
+        break;
+
+      case 'completed':
+        filteredItems = newProps.items.filter(i => !i.active)
+        break;
+
+      default: // 'active'
+        filteredItems = newProps.items.filter(i => i.active)
+        break;
+    }
+
+    return { filteredItems } // object to update state
+  }
+
+  state = {
+    filteredItems: [],
+    filter: 'active'
+  }
+
+  showCompletedItems = () => {
+    const filteredItems = this.props.items.filter(item => !item.active)
+    this.setState({ filteredItems, filter: 'completed' })
+  }
+
+  showActiveItems = () => {
+    const filteredItems = this.props.items.filter(item => item.active)
+    this.setState({ filteredItems, filter: 'active' })
+  }
+
+  showAllItems = () => {
+    const filteredItems = this.props.items
+    this.setState({ filteredItems, filter: 'all' })
+  }
+
   render() {
-    const { items } = this.props
-    const rows = items.map((item, idx) => (
-      <tr key={idx}>
-        <td><Link key={item.id} to={`/item/${item.id}`}>{item.title}</Link></td>
-        <td>{item.price}</td>
-        <td>{item.bids.length}</td>
-        <td>{item.active ? <Timer start={item.start} duration={item.duration} /> : 'Ended'}</td>
-      </tr>
+    console.log(this.props, this.state)
+    const { filteredItems } = this.state
+    const itemCards = filteredItems.map(item => (
+      <Link className="item__link" key={item.id} to={`/item/${item.id}`}>
+        <ItemCard item={item} />
+      </Link>
     ))
 
     return (
-      <table className="items__list">
-        <thead>
-          <tr>
-            <td>Title</td>
-            <td>Price</td>
-            <td>Number of bids</td>
-            <td>Time remaining</td>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
+      <div className="items__list">
+        <button onClick={this.showActiveItems}>Active</button>
+        <button onClick={this.showCompletedItems}>Completed</button>
+        <button onClick={this.showAllItems}>All</button>
+        {itemCards}
+      </div>
     )
   }
 }
